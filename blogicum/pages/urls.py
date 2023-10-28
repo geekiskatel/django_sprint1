1a507c6feb60700d4b792a84d486d88a74d6a1f5
geekiskatel <geekiskatel@gmail.com>
2023-10-28 19:20:16 +0300

    V3.0.0.

--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
 from .views import AboutView
+
 app_name = 'pages'
 
 urlpatterns = [
     path('pages/about/', AboutView.as_view(),
          name='about'),
-    path('pages/rules/', views.rules, name='rules'),
-    ]
+    path('pages/rules/', views.rules,
+         name='rules'),
+]
